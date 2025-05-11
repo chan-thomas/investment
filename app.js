98f05cec987c7053c8c16d4dcd92f07e79deeec2
Thomas Chan <96518381+chan-thomas@users.noreply.github.com>
2025-05-10 19:15:24 -0600

    added login page and fetching total saving

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({urlencoded:false}));
 
+app.get('/totalsaving', (req, res)=>{
+    res.json({"total":1500});
+})
+app.post('/login', (req, res)=>{
+    const {email, password} = req.body;
+    console.log(`email:${email} password:${password}`);
+    res.send(`Welcome: ${email}`);
+})
+
 app.listen(port, ()=>{
     console.log(`server is up. Listening on port:${port}`);
 })
