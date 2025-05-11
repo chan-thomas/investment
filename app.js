727f83e3ce50b773ce3af3346fa26cc8b0325546
Thomas Chan <96518381+chan-thomas@users.noreply.github.com>
2025-05-10 18:48:35 -0600

    added a dev script to monitor app.js and handle .env file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({urlencoded:false}));
 
 app.listen(port, ()=>{
     console.log(`server is up. Listening on port:${port}`);
