159a34f9c92384faf1923435e4bb960c89228b6b
Thomas Chan <96518381+chan-thomas@users.noreply.github.com>
2025-05-11 23:54:21 -0600

    wip

--- app.js
+++ app.js
@@ -9,11 +9,23 @@ app.use(express.urlencoded({urlencoded:false}));
 app.get('/totalsaving', (req, res)=>{
     res.json({"total":1500});
 })
+app.get('/login', (req, res) => {
+    //redirect to login page
+    res.redirect('/login.html');
+});
 app.post('/login', (req, res)=>{
     const {email, password} = req.body;
     console.log(`email:${email} password:${password}`);
     res.send(`Welcome: ${email}`);
 })
+app.get('/register', (req, res) => {
+    //redirect to register page
+    res.redirect('/register.html');
+});
+app.get('/profile', (req, res) => {
+    //redirect to profile page
+    res.redirect('/profile.html');
+});
 
 app.listen(port, ()=>{
     console.log(`server is up. Listening on port:${port}`);
